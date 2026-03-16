f51343870a99b1cfff8443364a1651cc6193434f
Javier <93495474+Javierob02@users.noreply.github.com>
2026-03-16 19:38:12 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const { startServer } = require('localtunnel-server');
+// server.js
+const localtunnel = require('localtunnel');
 
-const port = process.env.PORT || 3000;
+(async () => {
+  const port = process.env.PORT || 3000;
 
-startServer({
-  port,
-  host: '0.0.0.0',
-}).then(() => {
-  console.log(`LocalTunnel server running on port ${port}`);
-});
+  // start your own tunnel server
+  const tunnel = await localtunnel({ port, host: '0.0.0.0' });
+
+  console.log('LocalTunnel server running at URL:', tunnel.url);
+
+  tunnel.on('close', () => {
+    console.log('Tunnel closed');
+  });
+})();
